9d660518e16f903a0ad3f4207a548b3432f30004
CJ Eller <cjeller1592@gmail.com>
2021-02-22 10:57:49 -0500

    Add style to html and updated joke route

--- lambda_handler.py
+++ lambda_handler.py
@@ -7,19 +7,29 @@ app = Minik()
 def get_greetings():
     app.response.headers = {"Content-Type": "text/html; charset=utf-8"}
 
-    return """
-    <html>
+    return """    <html>
     <head>
-        <title>Hello EAB!</title>
+        <title>Hello World!</title>
+        <style>
+            body {
+            font-family: helvetica, arial, sans-serif;
+            margin: 2em;
+            }
+
+            h1 {
+            font-style: italic;
+            color: #0072ce;
+            }
+        </style>
     </head>
     <body>
-        <h1>Hello EAB!</h1>
-        <p>My name is CJ. I'd like to join the Cloud Ops Team</p>
+        <h1>Hello EAB</h1>
+        <p>My name is CJ. I'd like to join the Cloud Ops Team!</p>
     </body>
     </html>"""
 
-@app.get('/why/{reason}')
-def get_why(reason):
+@app.get('/joke/{subject}')
+def get_why(subject):
     # Get teh joke
     r = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
     joke = r.json()['joke']
@@ -29,11 +39,22 @@ def get_why(reason):
     return """
     <html>
     <head>
-        <title>Hello EAB!</title>
+        <title>Hello World!</title>
+        <style>
+        body {
+            font-family: helvetica, arial, sans-serif;
+            margin: 2em;
+            }
+
+        h1 {
+            font-style: italic;
+            color: #0072ce;
+            }
+        </style>
     </head>
     <body>
-        <p>Want a joke about %s?</p>
+        <h1>Want a joke about %s?</h1>
         <p>How about something else instead?</p>
         <p>%s</p>
     </body>
-    </html>""" % (reason, joke)
+    </html>""" % (subject, joke)
